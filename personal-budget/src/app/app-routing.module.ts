22fcd90b5f7692c231cb1f3ae43828243192d6d2
Ashwitha Lingannagari <ashwitharao51@gmail.com>
2023-09-24 21:36:36 -0400

    implemented Coding Exercise 02

--- personal-budget/src/app/app-routing.module.ts
+++ personal-budget/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path:'**',
     component: P404Component
+  },
+  {
+    path:'contact',
+    component: ContactComponent
   },
 ];
 
--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -1,6 +1,10 @@
-import { AfterViewInit, Component,  } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Chart } from 'chart.js/auto';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { DataService } from '../data.service';
+
+import  Chart  from 'chart.js/auto';
+import * as d3 from 'd3';
+
 
 
 @Component({
@@ -8,7 +12,7 @@ import { Chart } from 'chart.js/auto';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements AfterViewInit {
+export class HomepageComponent implements OnInit {
 
   public dataSource:any= {
     datasets: [
@@ -29,28 +33,88 @@ export class HomepageComponent implements AfterViewInit {
     labels: []
 };
 
+public newDataSource: any = [];
 
-  constructor(public http: HttpClient) {
-    const el = document.getElementById('mychart');
-    console.log('Is my chart there?', el);
+private svg: any;
+  private width = 650;
+  private height = 300;
+  // The radius of the pie chart is half the smallest side
+  private radius = Math.min(this.width, this.height) / 2;
+  private colors: any;
+  private pie: any;
+  private titles: string[] = [];
+
+constructor(private dataService: DataService) {}
+
+  private createSvg(): void {
+    this.svg = d3
+      .select('#pie-chart')
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .append('g')
+      .attr(
+        'transform',
+        'translate(' + this.width / 2 + ',' + this.height / 2 + ')'
+      );
   }
 
-  ngAfterViewInit(): void {
-    this.http.get('http://localhost:3000/budget')
-    .subscribe((res: any) => {
-      for (var i=0;i<res.myBudget.length; i++){
-          this.dataSource.datasets[0].data[i]=res.myBudget[i].budget;
-          this.dataSource.labels[i]=res.myBudget[i].title;
+  private createColors(): void {
+    const titles = this.dataSource.labels;
 
-      }
-      this.createChart()
-  });
+    this.colors = d3
+      .scaleOrdinal()
+      .domain(titles)
+      .range([
+        '#ffcd56',
+        '#ff6384',
+        '#36a2eb',
+        '#fd6b19',
+        '#83FF33',
+        '#F633FF',
+        '#FF3333',
+        '#a38080',
+      ]);
   }
+
+
+  private drawChart(): void {
+    // Compute the position of each group on the pie:
+    const pie = d3.pie<any>().value((d: any) => Number(d.budget));
+
+    // Build the pie chart
+    this.svg
+      .selectAll('pieces')
+      .data(pie(this.newDataSource))
+      .enter()
+      .append('path')
+      .attr('d', d3.arc().innerRadius(0).outerRadius(this.radius))
+      .attr('fill', (d: any, i: any) => this.colors(i))
+      .attr('stroke', '#121926')
+      .style('stroke-width', '1px');
+
+    // Add labels
+    const labelLocation = d3.arc().innerRadius(100).outerRadius(this.radius);
+
+    this.svg
+      .selectAll('pieces')
+      .data(pie(this.newDataSource))
+      .enter()
+      .append('text')
+      .text((d: any) => d.data.title)
+      .attr(
+        'transform',
+        (d: any) => 'translate(' + labelLocation.centroid(d) + ')'
+      )
+      .style('text-anchor', 'middle')
+      .style('font-size', 15);
+  }
+
 
   createChart() {
     // var ctx = document.getElementById("mychart").getContext("2d");
     var ctx = document.getElementById("myChart") as HTMLCanvasElement;
-    var myPiechart = new Chart (ctx, {
+    var myPieChart = new Chart (ctx, {
       type :'pie',
       data : this.dataSource
 
@@ -59,4 +123,30 @@ export class HomepageComponent implements AfterViewInit {
     });
 }
 
+ngOnInit(): void {
+  if (
+    this.dataSource.datasets[0].data.length == 0 ||
+    this.newDataSource.length == 0
+  ) {
+    this.dataService.fetchDataFromBackend().subscribe((res: any) => {
+      for (var i = 0; i < res.myBudget.length; i++) {
+        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+        this.dataSource.labels[i] = res.myBudget[i].title;
+
+        this.newDataSource.push({
+          title: res.myBudget[i].title,
+          budget: res.myBudget[i].budget,
+        });
+      }
+      this.dataService.setDataSource(this.dataSource);
+      this.dataService.setNewDataSource(this.newDataSource);
+
+      // console.log(this.dataService.getDataSource());
+      this.createChart();
+      this.createSvg();
+      this.createColors();
+      this.drawChart();
+    });
+  }
+}
 }
